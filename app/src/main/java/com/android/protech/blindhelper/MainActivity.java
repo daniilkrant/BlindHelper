5fc6b44bee497f2c872225d5e85cd2bb7f2a688d
Daniil_Krant <danbiil988@gmail.com>
2017-07-27 02:05:04 +0300

    initial

---
+++ app/src/main/java/com/android/protech/blindhelper/MainActivity.java
@@ -0,0 +1,84 @@
+package com.android.protech.blindhelper;
+
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.RemoteException;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import org.altbeacon.beacon.Beacon;
+import org.altbeacon.beacon.BeaconConsumer;
+import org.altbeacon.beacon.BeaconManager;
+import org.altbeacon.beacon.BeaconParser;
+import org.altbeacon.beacon.MonitorNotifier;
+import org.altbeacon.beacon.RangeNotifier;
+import org.altbeacon.beacon.Region;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+
+public class MainActivity extends AppCompatActivity implements BeaconConsumer {
+    private static final String TAG = "BEACON_PROJECT";
+    private ArrayList<String> beaconList;
+    private ListView beaconListView;
+    private ArrayAdapter<String> adapter;
+    private BeaconManager beaconManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        this.beaconList = new ArrayList<String>();
+        this.beaconListView = (ListView) findViewById(R.id.listView);
+        this.adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, this.beaconList);
+        this.beaconListView.setAdapter(adapter);
+        this.beaconManager = BeaconManager.getInstanceForApplication(this);
+        this.beaconManager.getBeaconParsers().add(new BeaconParser().
+                setBeaconLayout("m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24"));
+        this.beaconManager.bind(this);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        this.beaconManager.unbind(this);
+    }
+
+    @Override
+    public void onBeaconServiceConnect() {
+        this.beaconManager.setRangeNotifier(new RangeNotifier() {
+            @Override
+            public void didRangeBeaconsInRegion(Collection<Beacon> beacons, Region region) {
+                if (beacons.size() > 0) {
+                    beaconList.clear();
+                    for(Iterator<Beacon> iterator = beacons.iterator(); iterator.hasNext();) {
+                        beaconList.add("Distance: " + Double.toString(iterator.next().getDistance()));
+                    }
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            adapter.notifyDataSetChanged();
+                        }
+                    });
+                }
+            }
+        });
+        try {
+            this.beaconManager.startRangingBeaconsInRegion(new Region("MyRegionId", null, null, null));
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+    }
+}
