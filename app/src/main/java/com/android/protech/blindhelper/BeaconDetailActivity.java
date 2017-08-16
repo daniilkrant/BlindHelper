5e3be5ff0789185a5d6a4090078036c681f4caf1
daniilkrant <danbiil988@gmail.com>
2017-08-16 17:47:03 +0300

    Just commit Signed-off-by: daniilkrant <danbiil988@gmail.com>

---
+++ app/src/main/java/com/android/protech/blindhelper/BeaconDetailActivity.java
@@ -0,0 +1,13 @@
+package com.android.protech.blindhelper;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BeaconDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_beacon_detail);
+    }
+}
